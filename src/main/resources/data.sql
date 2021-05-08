22ed05cda9008a68c52cb92535233833f3124bc0
Srinivasan V <srinivas.fastrack@gmail.com>
2021-05-08 10:54:50 +0200

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'Srini', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'Srini', 'Learn Data JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'Srini', 'Learn Microservices', sysdate(), false);
